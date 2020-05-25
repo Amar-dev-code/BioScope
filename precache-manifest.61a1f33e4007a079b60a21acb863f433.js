b531f33752cbb935bc16792607b1eba538e7dcdb
amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>
2020-05-25 21:14:47 +0530

    Updates

---
+++ precache-manifest.61a1f33e4007a079b60a21acb863f433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc92749235b9a27b7ac12cbea69b61b",
+    "url": "/BioScope/index.html"
+  },
+  {
+    "revision": "e27ece6d7c55c374105e",
+    "url": "/BioScope/static/css/main.2f0f4404.chunk.css"
+  },
+  {
+    "revision": "a69f11e913e32355ccea",
+    "url": "/BioScope/static/js/2.e1b42f82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BioScope/static/js/2.e1b42f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27ece6d7c55c374105e",
+    "url": "/BioScope/static/js/main.a3cd1e16.chunk.js"
+  },
+  {
+    "revision": "4b5951d7fd528af8a8bc",
+    "url": "/BioScope/static/js/runtime-main.3629a954.js"
+  }
+]);
