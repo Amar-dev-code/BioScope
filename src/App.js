45b1d53bbe669dd830314b9d8724ebbd27473cb0
amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>
2020-05-25 21:58:22 +0530

    Fixing web look when app loads

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export function App() {
       <Router>
         <div>
           <Navbar />
-          <Route path="/" component={Landing} />
+          <Route exact path="/BioScope" component={Landing} />
           <Route exact path="/Test/:id" component={Test} />
           <Footer />
         </div>
